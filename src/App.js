724c081c41235c1f16deb1fd90035deccefa1c63
krish <krishdu.p@gmail.com>
2021-11-01 21:14:18 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Card extends React.Component {
+	render() {
+  	return (
+      <div className="container border border-warning">
+        <div className="row">
+          <div className="col-4">
+              <img src="https://avatars.githubusercontent.com/u/37765333?v=4" className="img-fluid rounded-pill" alt="profile"/>
+          </div>
+          <div className="col-8 mt-5">
+               <h5>Card title</h5>
+                <a href="/" className="btn btn-primary btn-sm">View Profile</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+class App extends React.Component {
+	render() {
+  	return (
+    	<div className="container">
+    	  <div className="text-center h3">{ this.props.title }</div>
+        <Card />
+    	</div>
+    );
+  }	
+}
+
 
 export default App;
