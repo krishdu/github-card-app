4a7bc4b4ff1d6e02d5f2ace1241d7a8487ab5877
krish <krishdu.p@gmail.com>
2021-11-02 11:25:56 +0530

    using static data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import './App.css';
 
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "Facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+
+//function component
+const CardList = (props) => (
+  <div>
+    {testData.map(profile => <Card {...profile}/>)}
+  </div>
+);
+
+//class component
 class Card extends React.Component {
 	render() {
+    const profile =this.props;
   	return (
       <div className="container border border-warning">
         <div className="row">
           <div className="col-4">
-              <img src="https://avatars.githubusercontent.com/u/37765333?v=4" className="img-fluid rounded-pill" alt="profile"/>
+              <img src={profile.avatar_url} className="img-fluid rounded-pill" alt="profile"/>
           </div>
           <div className="col-8 mt-5">
-               <h5>Card title</h5>
-                <a href="/" className="btn btn-primary btn-sm">View Profile</a>
+               <h5> {profile.name} </h5>
+               <h6> {profile.company} </h6>
           </div>
         </div>
       </div>
@@ -24,7 +40,7 @@ class App extends React.Component {
   	return (
     	<div className="container">
     	  <div className="text-center h3">{ this.props.title }</div>
-        <Card />
+        <CardList />
     	</div>
     );
   }	
